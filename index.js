f6670b94968eaf165e713fc246ba56ff95658268
Milind Patel <46854497+milindpatel11@users.noreply.github.com>
2023-11-04 17:29:40 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+let seedRule = 18;
+let genCycles = 8;   
+let cellPop = 15;
+let global_ruleset =  [0, 0, 0, 0, 0, 0, 0, 0];
+let first_live_cell = 7;
+const seedId = 'seed-rule';
+const seedId1 = 'seed-rule-1'
+const genId = 'num-gens' ;
+const popId = 'cell-pop';
+const first_live_id = 'first-live';
+
+//// TBA   TBA 
+//// TBA   TBA 
